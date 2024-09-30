fd1ef0a9bdfbbe5065be4287ec1fc7bf49339827
reset <reset>
2024-09-30 23:21:19 +0200

    refactor data collection

--- chrome/startpage/src/js/links.js
+++ chrome/startpage/src/js/links.js
@@ -1,36 +1,53 @@
-function fetchLinks(url) {
-  return fetch(url).then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    return response.json();
-  });
+const data = {
+  "/general": {
+    YouTube: "https://www.youtube.com/",
+    Gmail: "https://mail.google.com",
+    Instagram: "https://www.instagram.com",
+    Monkeytype: "https://monkeytype.com/",
+    Discord: "https://discord.com",
+  },
+  "/productivity": {
+    Office: "https://www.office.com/",
+    SatchelOne: "https://www.satchelone.com",
+    Outlook: "https://outlook.live.com",
+    Calendar: "https://calendar.google.com",
+    "Google Drive": "https://drive.google.com/drive/u/0/my-drive",
+  },
+  "/learning": {
+    Skillshare: "https://www.skillshare.com/",
+    Udemy: "https://www.udemy.com",
+    LinkedIn: "https://www.linkedin.com/learning/",
+    edX: "https://www.authn.edx.org/",
+    Educative: "https://www.educative.io",
+  },
+  "/revision": {
+    Seneca: "https://app.senecalearning.com/",
+    "P&MT": "https://www.physicsandmathstutor.com",
+    MathsGenie: "https://www/mathsgenie.co.uk/gcse.html",
+    FreeSciLessons: "https://www.freesciencelessons.co.uk/",
+    SaveMyExams: "https://www.savemyexams.co.uk",
+  },
+};
+
+const linksContainer = document.getElementById("links");
+for (const category in data) {
+  const categoryDiv = document.createElement("div");
+  categoryDiv.className = "urls";
+
+  const headerDiv = document.createElement("div");
+  headerDiv.className = "header";
+  headerDiv.textContent = category;
+  categoryDiv.appendChild(headerDiv);
+
+  const ul = document.createElement("ul");
+  for (const linkText in data[category]) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = data[category][linkText];
+    a.textContent = linkText;
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  categoryDiv.appendChild(ul);
+  linksContainer.appendChild(categoryDiv);
 }
-
-fetchLinks("../../links.json")
-  .catch(() => fetchLinks("../../links_example.json"))
-  .then((data) => {
-    const linksContainer = document.getElementById("links");
-    for (const category in data) {
-      const categoryDiv = document.createElement("div");
-      categoryDiv.className = "urls";
-
-      const headerDiv = document.createElement("div");
-      headerDiv.className = "header";
-      headerDiv.textContent = category;
-      categoryDiv.appendChild(headerDiv);
-
-      const ul = document.createElement("ul");
-      for (const linkText in data[category]) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.href = data[category][linkText];
-        a.textContent = linkText;
-        li.appendChild(a);
-        ul.appendChild(li);
-      }
-      categoryDiv.appendChild(ul);
-      linksContainer.appendChild(categoryDiv);
-    }
-  })
-  .catch((error) => console.error("Error fetching links:", error));
